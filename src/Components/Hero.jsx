f66e22646279c395b53d75516d47cee50d30445d
manuelthegoat <manuelcreation247@gmail.com>
2025-01-27 15:20:21 +0100

    push

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
             <div class="col-lg-12 emmanu">
               <div class="d-flex flex-column align-items-center justify-content-center h-100">
                 <h1
-                  class="position-relative text-color-dark text-5 line-height-5 font-weight-semibold px-4 mb-2 "
+                  class="position-relative text-color-light text-5 line-height-5 font-weight-semibold px-4 mb-2 "
                   data-appear-animation="fadeInDownShorterPlus"
                   data-plugin-options="{'minWindowWidth': 0}"
                 >
@@ -32,7 +32,7 @@ const Hero = () => {
                   </span>
                 </h1>
                 <h1
-                  class="text-color-dark font-weight-extra-bold text-12 mb-3 "
+                  class="text-color-light font-weight-extra-bold text-12 mb-3 "
                   data-appear-animation="blurIn"
                   data-appear-animation-delay="1000"
                   data-plugin-options="{'minWindowWidth': 0}"
@@ -40,7 +40,7 @@ const Hero = () => {
                   A NEW AND CLASSY FLAG?
                 </h1>
                 <p
-                  class="text-4 text-color-dark font-weight-light mb-0"
+                  class="text-4 text-color-light font-weight-light mb-0"
                   data-plugin-options="{'startDelay': 2000, 'minWindowWidth': 0}"
                 >
                   Check out our options and features
